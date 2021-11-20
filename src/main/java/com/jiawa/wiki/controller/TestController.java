3d6846fe8bda71c4a3d65e40ac3047f6f7f2498e
flfan <1490459938@qq.com>
2021-11-20 12:56:27 +0800

    config mysql

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String helloTest;
 
     @RequestMapping("/hello")
     public String hello() {
@@ -14,6 +18,6 @@ public class TestController {
 
     @PostMapping("/hello/post")
     public String helloPost(String name) {
-        return "hello post " + name;
+        return "hello post " + name +":" + helloTest;
     }
 }
