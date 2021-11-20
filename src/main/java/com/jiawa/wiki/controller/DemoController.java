46ce7924bf09d94a5500f733f47cd8c50029b5ab
flfan <1490459938@qq.com>
2021-11-20 16:30:24 +0800

    Add a general return class CommonResp, In the future, the return values of all back-end interfaces are all CommonResp type

--- src/main/java/com/jiawa/wiki/controller/DemoController.java
+++ src/main/java/com/jiawa/wiki/controller/DemoController.java
@@ -3,18 +3,20 @@ package com.jiawa.wiki.controller;
 import com.jiawa.wiki.domain.Demo;
 import com.jiawa.wiki.service.DemoService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
-public class demoController {
+@RequestMapping("/demo")
+public class DemoController {
 
     @Resource
     private DemoService demoService;
 
-    @GetMapping("/demo/list")
+    @GetMapping("/list")
     public List<Demo> demoMapper() {
         return demoService.list();
     }
